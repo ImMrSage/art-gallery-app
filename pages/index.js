ae81a2a579ceb95e737ca8e7fad4d35cb6829015
Philipp Gampe <tastycrysis@gmail.com>
2025-01-22 12:43:45 +0100

    add spotlight page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
-export default function HomePage() {
+import Spotlight from "@/components/Spotlight";
+import { useState, useEffect } from "react";
+export default function HomePage({ pieces }) {
+  const [randomPiece, setRandomPiece] = useState(null);
+
+  useEffect(() => {
+    setRandomPiece(pieces[Math.floor(Math.random() * pieces.length)]);
+  }, [pieces]);
+
+  if (!randomPiece) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <>
+      <h1>Art Gallery</h1>
+      <Spotlight
+        image={randomPiece.imageSource}
+        title={randomPiece.name}
+        artist={randomPiece.artist}
+      />
+    </>
   );
 }
