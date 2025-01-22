0ab31c094ba64a39ba753de7fcf02c936af3a5cc
Joshua Nuckel <joshuanuckel@icloud.com>
2025-01-22 10:48:14 +0100

    Finish fetch feature

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,25 @@
+import useSWR from "swr";
 import GlobalStyle from "../styles";
 
+const URL = "https://example-apis.vercel.app/api/art";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    const error = new Error("Error");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+  return res.json();
+};
+
 export default function App({ Component, pageProps }) {
+  const { data, error, isLoading } = useSWR(URL, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
   return (
     <>
       <GlobalStyle />
