a4f09074744909106f608d2256b63736ad81879b
Andrea Hohnsen <a.hohnsen@mail.de>
2025-01-23 17:13:42 +0100

    Add empty line

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Spotlight from "@/components/Spotlight";
 import { useState, useEffect } from "react";
+
 export default function HomePage({ pieces, toggleFavorite, favorites }) {
   const [randomPiece, setRandomPiece] = useState(null);
 
